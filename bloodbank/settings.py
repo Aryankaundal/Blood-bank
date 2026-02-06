7e76f4f1cbfcf115928d3bf464f2aebc5658e365
Aryan Kaundal <153354664+Aryankaundal@users.noreply.github.com>
2026-02-06 15:52:53 +0530

    Define BASE_DIR for static files in settings.py
    Added BASE_DIR definition for static files configuration.

--- bloodbank/settings.py
+++ bloodbank/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,6 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
