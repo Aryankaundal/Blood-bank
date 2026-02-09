7cbbaa514328b307f401e5f15448e408543d4cad
Aryan Kaundal <153354664+Aryankaundal@users.noreply.github.com>
2026-02-09 12:51:50 +0530

    Update settings.py

--- bloodbank/settings.py
+++ bloodbank/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0n=c$-0)^!iw0%eiy(04bqrgzze2+nq^e$ebp#95!&f6r6hv%x'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
